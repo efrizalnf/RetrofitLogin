05f442b25b3e2f18c70a97aa3f58b1661a55f12b
efrizalnf <ondabuzz9@gmail.com>
2019-10-11 19:23:13 +0700

    Test Retrofit

--- app/src/main/java/com/zlz/retrofitlogin/MainActivity.java
+++ app/src/main/java/com/zlz/retrofitlogin/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zlz.retrofitlogin;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private TextView mResponseTv;
+    private Button submitBtn;
     private Api mAPI;
     private Retrofit retrofit;
     private Login login;
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*final EditText titleEt = findViewById(R.id.et_title);
+        final EditText titleEt = findViewById(R.id.et_title);
         final EditText bodyEt = findViewById(R.id.et_body);
         final EditText latitude = findViewById(R.id.et_lat);
         final EditText longitude = findViewById(R.id.et_long);
@@ -42,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText appver = findViewById(R.id.et_appmodel);
         Button submitBtn = findViewById(R.id.btn_submit);
         mResponseTv = findViewById(R.id.tv_response);
-//        mAPI = ApiUtils.getApiService();
+
+        mAPI = ApiUtils.getApiService();
         mAPI = RetrofitClient.getClient(" ").create(Api.class);
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
@@ -58,79 +61,25 @@ public class MainActivity extends AppCompatActivity {
                 String model = modeldevice.getText().toString().trim();
                 String appor = appori.getText().toString().trim();
                 String appve = appver.getText().toString().trim();
-//                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(lat) && !TextUtils.isEmpty(longi) && !TextUtils.isEmpty(reg) && !TextUtils.isEmpty(ver) && !TextUtils.isEmpty(andro) && !TextUtils.isEmpty(model) && !TextUtils.isEmpty(appor) && !TextUtils.isEmpty(appve)) {
-                sendPost();
-//                }
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(lat) && !TextUtils.isEmpty(longi) && !TextUtils.isEmpty(reg) && !TextUtils.isEmpty(ver) && !TextUtils.isEmpty(andro) && !TextUtils.isEmpty(model) && !TextUtils.isEmpty(appor) && !TextUtils.isEmpty(appve)) {
+                sendPost(email,pass,lat,longi,reg, ver, andro, model,appor, appve);
+                }
             }
-        });*/
+        });
 
         mAPI = RetrofitClient.getClient(" ").create(Api.class);
-        sendPost();
+//        sendPost();
     }
 
-//    public void sendPost() {
-////        mAPI.saveData("moderator@jababeka.com", "12345", "10.89823", "909993", "123", "234", "2", "32", "2", "1"){
-//        Call<StatusLogin<Login>> loginCall = mAPI.saveData("moderator@jababeka.com", "12345", "10.89823", "909993", "123", "234", "2", "32", "2", "1");
-//        {
-//            {
-//                loginCall.enqueue(new retrofit<StatusLogin<Login>>() {
-//
-//                    @Override
-//                    public void onResponse(Call<Login> call, Response<Login> response) {
-//                        if (response.isSuccessful()) {
-//                            showResponse(response.body().toString());
-//                            Log.i("post submit api", response.body().toString());
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<Login> call, Throwable t) {
-//
-//                    }
-//
-//                });
-//            }
-//
-//            public void showResponse(String response){
-//            if (mResponseTv.getVisibility() == View.GONE) {
-//                mResponseTv.setVisibility(View.VISIBLE);
-//            }
-//            mResponseTv.setText(response);
-//
-//        }
-//        }
-//    }
-
-
-    public void sendPost() {
-//        mAPI.saveData("moderator2@jababeka.com", "12345", " ", " ", " ", " ", " ", " ", " ", " ").enqueue(new Callback<StatusLogin() {
-//            @Override
-//            public void onResponse(Call<StatusLogin> call, Response<StatusLogin<Login>> response) {
-//                if (response.isSuccessful()) {
-//                    showResponse(response.body().getData().getEmail().toString());
-//                    showResponse(response.body().getData().getFoto().toString());
-//                    showResponse(response.body().getData().getToken().toString());
-//                    showResponse(response.body().getData().getAkses().toString());
-//                    showResponse(response.body().getData().getIsInternal().toString());
-//                    Log.i("pos :", response.body().getData().getIsInternal().toString());
-////                Log.i("pos :", response.body().getData().getFoto().toString());
-////                Log.i("pos :",response.body().getData().getToken().toString());
-////                Log.i("pos :", response.body().getData().getAkses().toString());
-////                Log.i("pos :", response.body().getData().getIsInternal().toString());
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<StatusLogin<Login>> call, Throwable t) {
-//                t.printStackTrace();
-//            }
-//        });
-        mAPI.saveData("moderator2@jababeka.com", "12345", " ", " ", " ", " ", " ", " ", " ", " ").enqueue(new Callback<StatusLogin>() {
+    public void sendPost(String email,String pass,String lat,String longi,String reg,String ver,String andro,String model,String appor,String appve) {
+        mAPI.saveData("zalzondabuzz@gmail.com", "Qwerty2!", " ", " ", " ", " ", " ", " ", " ", " ").enqueue(new Callback<StatusLogin>() {
             @Override
-            public void onResponse(Call<StatusLogin> call, Response<StatusLogin> response) {
+            public void onResponse(Call<StatusLogin> call, Response<StatusLogin> response){
                 Gson gson = new Gson();
                 Log.d(TAG, "onResponse: " + gson.toJson(response.body()));
+                Log.i(TAG, "post" + response.body().toString());
+
+                showResponse(gson.toJson(response.body()));
             }
 
             @Override
@@ -146,30 +95,5 @@ public class MainActivity extends AppCompatActivity {
         }
         mResponseTv.setText(response);
     }
-
-//    public void sendPost() {
-//        mAPI.saveData("moderator2@jababeka.com", "12345", "10.89823", "909993", "123", "234", "2", "32", "2", "1").enqueue(new Callback<StatusLogin<L>>() {
-//            @Override
-//            public void onResponse(Call<StatusLogin<L>> call, Response<StatusLogin<L>> response) {
-//                if (response.isSuccessful()){
-//                    showResponse(response.body().toString());
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<StatusLogin<L>> call, Throwable t) {
-//
-//            }
-//
-//        });
-//    }
-//
-//    public void showResponse(String response) {
-//        if (mResponseTv.getVisibility() == View.GONE) {
-//            mResponseTv.setVisibility(View.VISIBLE);
-//        }
-//        mResponseTv.setText(response);
-//    }
-
 
 }
